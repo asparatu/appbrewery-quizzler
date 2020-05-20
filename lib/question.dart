c07e4f318cfdac57ccea323e8294ba58a31eb24c
asparatu <asparatu@gmail.com>
2020-05-20 09:29:16 +0800

    Update Question Class

--- lib/question.dart
+++ lib/question.dart
@@ -1,1 +1,14 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
 
+  Question({String q, bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+
+  @override
+  String toString() {
+    return '{ ${this.questionText}, ${this.questionAnswer} }';
+  }
+}
